09843b3bb296ba75cea5c6f194c80a364d171538
Quickz <alexserg@inbox.lv>
2023-03-15 23:47:29 +0200

    got rid of a comment

--- svelte-dotnet-template/Client/src/routes/models/WeatherForecast.ts
+++ svelte-dotnet-template/Client/src/routes/models/WeatherForecast.ts
@@ -3,7 +3,6 @@ export class WeatherForecast
     public date : Date = new Date();
     public temperatureC : number = 0;
     public summary : string = "";
-    // public TemperatureF : number => ;
     public get temperatureF() : number
     {
         return 32 + Math.floor(this.temperatureC / 0.5556);
