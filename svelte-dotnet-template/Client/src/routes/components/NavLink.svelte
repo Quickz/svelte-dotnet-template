1d0b642e1e524bc728127ed2191df694b8eef341
Quickz <alexserg@inbox.lv>
2023-12-27 00:40:42 +0200

    Fixed navigation related UI issue (seems to be broken after updating packages, updated it to use SvelteKit stuff for listening for navigational changes)

--- svelte-dotnet-template/Client/src/routes/components/NavLink.svelte
+++ svelte-dotnet-template/Client/src/routes/components/NavLink.svelte
@@ -1,60 +1,7 @@
 <script lang="ts">
-    import { onDestroy, onMount } from "svelte";
-
-    let activeClass = "";
-    let stopListening : Function;
-
-    onMount(() =>
-    {
-        updateState();
-        stopListening = startListening();
-    });
-
-    onDestroy(() =>
-    {
-        if (stopListening)
-            stopListening();
-    })
-
-    function startListening()
-    {
-        const originalPushState = history.pushState;
-        const originalReplaceState = history.replaceState;
-        history.pushState = function () {
-            originalPushState.apply(this, arguments);
-            updateState();
-        };
-        history.replaceState = function () {
-            originalReplaceState.apply(this, arguments);
-            updateState();
-        };
-        window.addEventListener('popstate', updateState);
-        window.addEventListener('hashchange', updateState);
-
-        return function()
-        {
-            history.pushState = originalPushState;
-            history.replaceState = originalReplaceState;
-            window.removeEventListener('popstate', updateState);
-            window.removeEventListener('hashchange', updateState);
-        };
-    }
-
-    function updateState()
-    {
-        let currentHref = $$props.href;
-        var url = new URL(window.location.href);
-        
-        if (currentHref == url.href ||
-            currentHref == url.pathname)
-        {
-            activeClass = "active";
-        }
-        else
-        {
-            activeClass = "";
-        }
-    }
+    import { page } from '$app/stores';
+    
+    $: activeClass = $$props.href == $page.url.pathname ? "active" : "";
 </script>
 
 <a class="{$$props.class} {activeClass}" href={$$props.href}>
