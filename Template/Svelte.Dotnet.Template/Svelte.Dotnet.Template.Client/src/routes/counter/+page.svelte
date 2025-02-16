a0f805766fc58ef49029cd950e2b21332acd21a3
Quickz <alexserg@inbox.lv>
2025-02-16 23:06:08 +0200

    removed an unnecessary comment

--- Template/Svelte.Dotnet.Template/Svelte.Dotnet.Template.Client/src/routes/counter/+page.svelte
+++ Template/Svelte.Dotnet.Template/Svelte.Dotnet.Template.Client/src/routes/counter/+page.svelte
@@ -16,23 +16,3 @@
 <p role="status">Current count: {currentCount}</p>
 
 <button class="btn btn-primary" on:click={incrementCount}>Click me</button>
-
-
-
-
-<!-- <script>
-    let count = 0;
-
-    function increment()
-    {
-        count++;
-    }
-</script>
-
-<h2>Counter</h2>
-<div>
-    Current count: {count}
-</div>
-<div>
-    <button class="btn btn-primary" on:click={increment}>Click me</button>
-</div> -->
