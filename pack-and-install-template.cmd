9288d05d69c349bf572a898819d7c26ec87b4841
Quickz <alexserg@inbox.lv>
2025-05-11 18:47:17 +0300

    updated version number

--- pack-and-install-template.cmd
+++ pack-and-install-template.cmd
@@ -2,5 +2,5 @@ cd Template
 dotnet pack Svelte.Dotnet.Template.csproj
 cd bin/Release
 dotnet new uninstall Svelte.Dotnet.Template
-dotnet new install Svelte.Dotnet.Template.1.0.1.nupkg
+dotnet new install Svelte.Dotnet.Template.1.0.2.nupkg
 pause
